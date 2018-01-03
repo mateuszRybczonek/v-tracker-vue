12fd201a31e25dbdc2152ff7a2d5f532f78b4035
Mateusz Rybczonek <mateusz.rybczonek@netguru.pl>
2018-01-03 16:57:23 +0100

    add vessel-details route and page

---
+++ src/pages/vessel-details.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="vessel-details">
+    <h1>Vessel details</h1>
+  </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+
+  export default {
+    created () {
+      this.$store.dispatch('fetchReports')
+    },
+
+    computed: { //  map getter to property
+      ...mapGetters([
+        'vessels',
+        'reports'
+      ])
+    },
+
+    components: {
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  h1, p {
+    text-align: center;
+  }
+
+  h1 {
+    margin: 50px 0;
+  }
+</style>
