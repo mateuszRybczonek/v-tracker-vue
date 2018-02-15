2488623dc8bf6bc864171c7ca0d1ecdc3e2ee189
Mateusz Rybczonek <mateusz.rybczonek@netguru.pl>
2018-02-15 14:36:38 +0100

    extract success sign to seprate component

---
+++ src/components/molecules/success-sign.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="success-sign">
+    <div class="circle">
+      <span class="checkmark draw"></span>
+    </div>
+    <h3>{{title}}</h3>
+    <p>{{description}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      title: {
+        type: String,
+        required: true
+      },
+      description: {
+        type: String,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .success-sign {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    $loader-size: 8em;
+    $check-height: $loader-size/2;
+    $check-width: $check-height/2;
+    $check-left: ($loader-size/6 + $loader-size/12);
+    $check-thickness: 3px;
+    $check-color: $color-light-blue;
+
+    .circle {
+      margin: 0 0 30px 10px;
+      border: $check-thickness solid rgba(0, 0, 0, 0.2);
+      border-left-color: $check-color;
+      position: relative;
+    }
+
+    .circle,
+    .circle:after {
+      border-radius: 50%;
+      width: $loader-size;
+      height: $loader-size;
+      border-color: $check-color;
+      transition: border 2s ease-out;
+    }
+
+    .checkmark {
+      &.draw:after {
+        animation-duration: 2000ms;
+        animation-timing-function: ease;
+        animation-name: checkmark;
+        transform: scaleX(-1) rotate(135deg);
+      }
+
+      &:after {
+        opacity: 1;
+        height: $check-height;
+        width: $check-width;
+        transform-origin: left top;
+        border-right: $check-thickness solid $check-color;
+        border-top: $check-thickness solid $check-color;
+        content: '';
+        left: $check-left;
+        top: $check-height;
+        position: absolute;
+      }
+    }
+
+    @keyframes checkmark {
+      0% {
+        height: 0;
+        width: 0;
+        opacity: 1;
+      }
+      20% {
+        height: 0;
+        width: $check-width;
+        opacity: 1;
+      }
+      40% {
+        height: $check-height;
+        width: $check-width;
+        opacity: 1;
+      }
+      100% {
+        height: $check-height;
+        width: $check-width;
+        opacity: 1;
+      }
+    }
+  }
+</style>
--- src/components/vessel-details/reports/new-report/form/Step4.vue
+++ src/components/vessel-details/reports/new-report/form/Step4.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="step-4">
-    <div class="circle">
-      <span class="checkmark draw"></span>
-    </div>
-    <h3>Success!</h3>
-    <p>New report has been added!</p>
+    <success-sign
+      title="Success!"
+      description="New report has been added!"
+    ></success-sign>
   </div>
 </template>
 
 <script>
+  import SuccessSign from '../../../../molecules/success-sign.vue'
+
   export default {
+    components: {
+      SuccessSign
+    }
   }
 </script>
 
@@ -21,72 +25,5 @@
     justify-content: center;
     align-items: center;
     flex-direction: column;
-    $loader-size: 8em;
-    $check-height: $loader-size/2;
-    $check-width: $check-height/2;
-    $check-left: ($loader-size/6 + $loader-size/12);
-    $check-thickness: 3px;
-    $check-color: $color-light-blue;
-
-    .circle {
-      margin: 0 0 30px 10px;
-      border: $check-thickness solid rgba(0, 0, 0, 0.2);
-      border-left-color: $check-color;
-      position: relative;
-    }
-
-    .circle,
-    .circle:after {
-      border-radius: 50%;
-      width: $loader-size;
-      height: $loader-size;
-      border-color: $check-color;
-      transition: border 2s ease-out;
-    }
-
-    .checkmark {
-      &.draw:after {
-        animation-duration: 2000ms;
-        animation-timing-function: ease;
-        animation-name: checkmark;
-        transform: scaleX(-1) rotate(135deg);
-      }
-
-      &:after {
-        opacity: 1;
-        height: $check-height;
-        width: $check-width;
-        transform-origin: left top;
-        border-right: $check-thickness solid $check-color;
-        border-top: $check-thickness solid $check-color;
-        content: '';
-        left: $check-left;
-        top: $check-height;
-        position: absolute;
-      }
-    }
-
-    @keyframes checkmark {
-      0% {
-        height: 0;
-        width: 0;
-        opacity: 1;
-      }
-      20% {
-        height: 0;
-        width: $check-width;
-        opacity: 1;
-      }
-      40% {
-        height: $check-height;
-        width: $check-width;
-        opacity: 1;
-      }
-      100% {
-        height: $check-height;
-        width: $check-width;
-        opacity: 1;
-      }
-    }
   }
 </style>
