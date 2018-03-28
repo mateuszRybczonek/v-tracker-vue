d67da7dd9438b4c7052089a02f39910ff533d81a
Mateusz Rybczonek <mateusz.rybczonek@netguru.pl>
2018-03-28 10:46:36 +0200

    fix icons

--- src/components/icons/icon-overview.vue
+++ src/components/icons/icon-overview.vue
@@ -1,6 +1,8 @@
 <template>
-  <rect x="20" y="83" style="stroke:black;stroke-width:1px;" width="15" height="15"  ry="0" rx="0"/>
-  <rect x="40" y="83" style="stroke:black;stroke-width:1px;" width="15" height="15"  ry="0" rx="0"/>
-  <rect x="20" y="103" style="stroke:black;stroke-width:1px;" width="15" height="15"  ry="0" rx="0"/>
-  <rect x="40" y="103" style="stroke:black;stroke-width:1px;" width="15" height="15"  ry="0" rx="0"/>
+  <g>
+    <rect x="23" y="20" style="stroke:black;stroke-width:1px;" width="15" height="15"  ry="0" rx="0"/>
+    <rect x="43" y="20" style="stroke:black;stroke-width:1px;" width="15" height="15"  ry="0" rx="0"/>
+    <rect x="23" y="40" style="stroke:black;stroke-width:1px;" width="15" height="15"  ry="0" rx="0"/>
+    <rect x="43" y="40" style="stroke:black;stroke-width:1px;" width="15" height="15"  ry="0" rx="0"/>
+  </g>
 </template>
--- src/components/icons/icon-statistics.vue
+++ src/components/icons/icon-statistics.vue
@@ -1,6 +1,8 @@
 <template>
-  <rect x="21" y="110" style="stroke:black;stroke-width:1px;" width="40" height="5"  ry="0" rx="0"/>
-  <rect x="35" y="82" style="stroke:black;stroke-width:1px;" width="10" height="25"  ry="0" rx="0"/>
-  <rect x="48" y="87" style="stroke:black;stroke-width:1px;" width="10" height="20"  ry="0" rx="0"/>
-  <rect x="23" y="97" style="stroke:black;stroke-width:1px;" width="10" height="10"  ry="0" rx="0"/>
+  <g>
+    <rect x="20" y="43" style="stroke:black;stroke-width:1px;" width="40" height="5"  ry="0" rx="0"/>
+    <rect x="34" y="15" style="stroke:black;stroke-width:1px;" width="10" height="25"  ry="0" rx="0"/>
+    <rect x="47" y="20" style="stroke:black;stroke-width:1px;" width="10" height="20"  ry="0" rx="0"/>
+    <rect x="22" y="30" style="stroke:black;stroke-width:1px;" width="10" height="10"  ry="0" rx="0"/>
+  </g>
 </template>
