64dadf95669e78c02d1b1d75d0a18281b8bbdfe3
Mateusz Rybczonek <mateusz.rybczonek@netguru.pl>
2018-04-16 17:33:41 +0200

    NavigationData unit test

---
+++ test/stubs/report.js
@@ -0,0 +1,25 @@
+const report = {
+  course: 90,
+  createdAt: '2018-03-19T15:38:34.255Z',
+  createdBy: 'PUBx21545ZOytG9KcWyaPZjSQyK2',
+  doRob: 100,
+  foRob: 90,
+  fwRob: 80,
+  id: '-L7yuO7nLZtMgEe8qth6',
+  lat: 12.338,
+  lng: 12.34,
+  pitch: 1,
+  pob: 67,
+  reportTime: '2018-02-10',
+  roll: 0.3,
+  seaState: 2,
+  spd: 12,
+  swellDir: 100,
+  swellHeight: 4,
+  updatedAt: '',
+  vessel: '-L2sqMuqy29K5adoAZdT',
+  windDir: 100,
+  windSpd: 40
+}
+
+export { report }
---
+++ test/unit/specs/NavigationData.spec.js
@@ -0,0 +1,22 @@
+import { mount } from 'vue-test-utils'
+import NavigationData from '@/components/NavigationData'
+import BaseBadge from '@/components/BaseBadge'
+import BaseList from '@/components/BaseList'
+import { report } from '../../stubs/report'
+
+describe('NavigationData.vue', () => {
+  const wrapper = mount(NavigationData, {
+    propsData: {
+      report,
+      fetchingReports: false
+    }
+  })
+
+  it('renders BaseBadge component', () => {
+    expect(wrapper.findAll(BaseBadge)).toHaveLength(1)
+  })
+
+  it('renders BaseList component', () => {
+    expect(wrapper.findAll(BaseList)).toHaveLength(1)
+  })
+})
