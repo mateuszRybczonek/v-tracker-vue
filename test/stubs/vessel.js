1916691d8254b7e3f8cd0fb6944c531ba3891999
Mateusz Rybczonek <mateusz.rybczonek@netguru.pl>
2018-04-17 10:14:27 +0200

    add Vuex - vessel mutations spec

---
+++ test/stubs/vessel.js
@@ -0,0 +1,37 @@
+const firstVessel = {
+  flag: 'Marshall Islands',
+  gt: '47000',
+  id: '1',
+  imoNumber: '123456789',
+  mmsi: '987654321',
+  name: 'ENSCO DS-2',
+  owner: 'o1',
+  owners: {
+    'o1': true
+  },
+  reports: {
+    'r1': true,
+    'r2': true
+  },
+  yob: '1997'
+}
+
+const secondVessel = {
+  flag: 'Poland',
+  gt: '45000',
+  id: '2',
+  imoNumber: '987654321',
+  mmsi: '987654324',
+  name: 'ENSCO DS-8',
+  owner: 'o2',
+  owners: {
+    'o2': true
+  },
+  reports: {
+    'r3': true,
+    'r4': true
+  },
+  yob: '2013'
+}
+
+export { firstVessel, secondVessel }
