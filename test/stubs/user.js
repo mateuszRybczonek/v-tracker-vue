e6d1b0f50a4ce68fb860e7006d9320cbd0559223
Mateusz Rybczonek <mateusz.rybczonek@netguru.pl>
2018-04-24 15:52:38 +0200

    user stub

---
+++ test/stubs/user.js
@@ -0,0 +1,17 @@
+const userData = {
+  userEmail: "sample@gmail.com",
+  userId: "PUBLIC",
+  token: '111',
+}
+
+const user = {
+  confirmPassword: "987654321",
+  email: "sample@gmail.com",
+  password: "987654321",
+  userId: "PUBLIC",
+  vessels: {
+    '333': true
+  }
+}
+
+export { userData, user }
