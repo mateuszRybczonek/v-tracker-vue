eb8f387eae59cc2e45b6024dacc115c73e33e5e3
Mateusz Rybczonek <mateusz.rybczonek@netguru.pl>
2018-05-11 11:59:48 +0200

    VesselsList unit tests

---
+++ src/components/__tests__/VesselsList.spec.js
@@ -0,0 +1,29 @@
+import { mount } from 'vue-test-utils'
+import VesselsList from '@/components/VesselsList.vue'
+import VesselsListItem from '@/components/VesselsListItem.vue'
+import VesselsListItemNew from '@/components/VesselsListItemNew.vue'
+import { firstVessel, secondVessel } from '@/../test/stubs/vessel'
+
+describe('VesselsList.vue', () => {
+  const setup = () => {
+    const wrapper = mount(VesselsList, {
+      propsData: {
+        vessels: [firstVessel, secondVessel]
+      }
+    })
+
+    return { wrapper }
+  }
+
+  describe('it renders', () => {
+    const { wrapper } = setup()
+
+    test('with VesselsListItemNew component', () => {
+      expect(wrapper.findAll(VesselsListItemNew)).toHaveLength(1)
+    })
+
+    test('with proper number of VesselsListItem component', () => {
+      expect(wrapper.findAll(VesselsListItem)).toHaveLength(2)
+    })
+  })
+})
