ffbead4a751a0bf6d34bb609a226f863d884abe6
Mateusz Rybczonek <mateusz.rybczonek@netguru.pl>
2018-05-16 10:29:11 +0200

    NewVesselPage unit test

--- src/pages/NewVesselPage.vue
+++ src/pages/NewVesselPage.vue
@@ -1,7 +1,10 @@
 <template>
   <FormWrapper class="new-vessel-form" title="Create new vessel" size="narrow">
     <form slot="content" @submit.prevent>
-      <input class="new-vessel-form__name-input input--with-error" :class="{ invalid: showErrors && $v.vessel.name.$invalid }"
+      <input
+        data-test-edit-vessel-form-name-input
+        class="new-vessel-form__name-input input--with-error"
+        :class="{ invalid: showErrors && $v.vessel.name.$invalid }"
         @blur="$v.vessel.name.$touch()"
         v-model="vessel.name"
         placeholder="Name">
@@ -9,7 +12,10 @@
         <span class="validation-error" v-if="showErrors && !$v.vessel.name.required">This field must not be empty.</span>
       </div>
 
-      <input class="new-vessel-form__imo-number-input input--with-error" :class="{ invalid: showErrors && $v.vessel.imoNumber.$invalid }"
+      <input
+        data-test-edit-vessel-form-imo-input
+        class="new-vessel-form__imo-number-input input--with-error"
+        :class="{ invalid: showErrors && $v.vessel.imoNumber.$invalid }"
         @blur="$v.vessel.imoNumber.$touch()"
         v-model="vessel.imoNumber"
         placeholder="IMO Number">
@@ -18,14 +24,19 @@
         <span class="validation-error" v-if="showErrors && !$v.vessel.imoNumber.numeric">IMO number must be a number.</span>
       </div>
 
-      <input class="new-vessel-form__callsign-input input--with-error"
+      <input
+        data-test-edit-vessel-form-callsign-input
+        class="new-vessel-form__callsign-input input--with-error"
         v-model="vessel.callsign"
         placeholder="Call sign">
       <div class="error">
         <span class="validation-error"></span>
       </div>
 
-      <input class="new-vessel-form__mmsi-input input--with-error" :class="{ invalid: showErrors && $v.vessel.mmsi.$invalid }"
+      <input
+        data-test-edit-vessel-form-mmsi-input
+        class="new-vessel-form__mmsi-input input--with-error"
+        :class="{ invalid: showErrors && $v.vessel.mmsi.$invalid }"
         @blur="$v.vessel.mmsi.$touch()"
         v-model="vessel.mmsi"
         placeholder="MMSI">
@@ -33,7 +44,10 @@
         <span class="validation-error" v-if="showErrors && !$v.vessel.mmsi.numeric">MMSI must be a number.</span>
       </div>
 
-      <input class="new-vessel-form__gt-input input--with-error" :class="{ invalid: showErrors && $v.vessel.gt.$invalid }"
+      <input
+        data-test-edit-vessel-form-gt-input
+        class="new-vessel-form__gt-input input--with-error"
+        :class="{ invalid: showErrors && $v.vessel.gt.$invalid }"
         @blur="$v.vessel.gt.$touch()"
         v-model="vessel.gt"
         placeholder="Gross tonnage">
@@ -41,7 +55,10 @@
         <span class="validation-error" v-if="showErrors && !$v.vessel.gt.numeric">GT must be a number.</span>
       </div>
 
-      <input class="new-vessel-form__yob-input input--with-error" :class="{ invalid: showErrors && $v.vessel.yob.$invalid }"
+      <input
+        data-test-edit-vessel-form-yob-input
+        class="new-vessel-form__yob-input input--with-error"
+        :class="{ invalid: showErrors && $v.vessel.yob.$invalid }"
         @blur="$v.vessel.yob.$touch()"
         v-model="vessel.yob"
         placeholder="Year of build">
@@ -49,7 +66,10 @@
         <span class="validation-error" v-if="showErrors && !$v.vessel.yob.numeric">Year of build must be a number.</span>
       </div>
 
-      <input class="new-vessel-form__flag-input input--with-error" :class="{ invalid: showErrors && $v.vessel.flag.$invalid }"
+      <input
+        data-test-edit-vessel-form-flag-input
+        class="new-vessel-form__flag-input input--with-error"
+        :class="{ invalid: showErrors && $v.vessel.flag.$invalid }"
         @blur="$v.vessel.flag.$touch()"
         v-model="vessel.flag"
         placeholder="Flag">
