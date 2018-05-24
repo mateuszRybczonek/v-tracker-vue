2394a4109cea7c7de3c6bd2c886c4dc40ab78d7e
Mateusz Rybczonek <mateusz.rybczonek@netguru.pl>
2018-05-24 21:24:03 +0200

    fix CalendarCar and ReportSelector

--- src/components/__tests__/ReportSelector.spec.js
+++ src/components/__tests__/ReportSelector.spec.js
@@ -1,16 +1,42 @@
-import { mount } from 'vue-test-utils'
+import { mount, createLocalVue } from 'vue-test-utils'
 import ReportSelector from '@/components/ReportSelector'
 import CalendarCard from '@/components/CalendarCard'
 import { Carousel, Slide } from 'vue-carousel'
 import { report, secondReport } from '@/../test/stubs/report'
+import Vuex from 'vuex'
+import moment from 'moment'
+import VueMomentJS from 'vue-momentjs'
 
 describe('ReportSelector.vue', () => {
-  const wrapper = mount(ReportSelector, {
-    propsData: {
-      reports: [report, secondReport],
-      fetchingReports: false
+  const setup = () => {
+    const getters = {
+      selectedReport: jest.fn()
     }
-  })
+
+    getters.selectedReport.mockReturnValue(report)
+
+    const localVue = createLocalVue()
+    localVue.use(Vuex)
+    localVue.use(VueMomentJS, moment)
+
+    const store = new Vuex.Store({
+      state: {},
+      getters
+    })
+
+    const wrapper = mount(ReportSelector, {
+      localVue,
+      store,
+      propsData: {
+        reports: [report, secondReport],
+        fetchingReports: false
+      }
+    })
+
+    return { wrapper }
+  }
+
+  const { wrapper } = setup()
 
   it('renders Carousel component', () => {
     expect(wrapper.findAll(Carousel)).toHaveLength(1)
