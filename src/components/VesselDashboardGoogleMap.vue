b7d28484e69ffb11503c1c14f058df1b586edd3f
Mateusz Rybczonek <mateusz.rybczonek@netguru.pl>
2018-06-07 09:26:55 +0200

    use pathify getters in VesselDashboard and VesselDashboardGoogleMap

--- src/components/VesselDashboardGoogleMap.vue
+++ src/components/VesselDashboardGoogleMap.vue
@@ -54,6 +54,7 @@ import {
 } from '@/constants/mapSettings'
 
 import { mapGetters, mapActions } from 'vuex'
+import { get } from 'vuex-pathify'
 
 import {
   mapReportsToMarkers,
@@ -93,12 +94,13 @@ export default {
 
   computed: {
     ...mapGetters([
-      'reports',
       'sortedReports',
-      'fetchingReports',
-      'selectedReport',
       'dashboardGoogleMap'
     ]),
+
+    fetchingReports: get('fetchingReports'),
+    selectedReport: get('selectedReport'),
+    reports: get('reports'),
 
     newSelectedReport () {
       if(this.googleMapMarkers.length > 0) return this.selectedReport
