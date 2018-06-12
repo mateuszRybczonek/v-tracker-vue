356fcc62aaeffdf03adb0fdd3351547cd86b0475
Mateusz Rybczonek <mateusz.rybczonek@netguru.pl>
2018-06-12 14:47:28 +0200

    add colors to Doughnuts

--- src/components/DoughnutChartRemainingOnBoard.vue
+++ src/components/DoughnutChartRemainingOnBoard.vue
@@ -14,7 +14,14 @@
 import DoughnutChart from '@/components/DoughnutChart'
 import colorPalette from '@/constants/colorPalette'
 
-const { COLOR_BLACK, COLOR_BROWN, COLOR_BLUE, COLOR_WHITE } = colorPalette
+const {
+  COLOR_BLACK,
+  COLOR_BROWN,
+  COLOR_BLUE,
+  COLOR_WHITEY,
+  COLOR_GREEN,
+  COLOR_TOMATO
+} = colorPalette
 
 export default {
   components: {
@@ -33,6 +40,9 @@ export default {
     change: {
       type: Number,
       requried: true
+    },
+    color: {
+      type: String
     }
   },
 
@@ -51,8 +61,36 @@ export default {
               this.change,
               this.empty
             ],
+            backgroundColor: this.colorSet
           }
         ]
+      }
+    },
+
+    changeColor () {
+      return this.change > 0 ? COLOR_GREEN : COLOR_TOMATO
+    },
+
+    colorSet () {
+      switch (this.color) {
+        case 'black':
+          return [
+            COLOR_BLACK,
+            this.changeColor,
+            COLOR_WHITEY
+          ]
+        case 'brown':
+          return [
+            COLOR_BROWN,
+            this.changeColor,
+            COLOR_WHITEY
+          ]
+        case 'light-blue':
+          return [
+            COLOR_BLUE,
+            this.changeColor,
+            COLOR_WHITEY
+          ]
       }
     }
   }
