9ea14b1fbe24f5a70a827d0a4246e2fc558297be
Mateusz Rybczonek <mateusz.rybczonek@netguru.pl>
2018-06-12 09:33:15 +0200

    DoughnutChart created

---
+++ src/components/DoughnutChart.vue
@@ -0,0 +1,33 @@
+<script>
+import { Doughnut } from 'vue-chartjs'
+
+export default {
+  extends: Doughnut,
+
+  props: {
+    chartData: {
+      type: Object,
+      required: true
+    }
+  },
+
+  mounted () {
+    const labels = this.chartData.labels
+    const datasets = this.chartData.datasets
+
+    this.renderChart(
+      {
+        labels,
+        datasets
+      },
+      {
+        responsive: true,
+        maintainAspectRatio: false,
+        legend: {
+          display: false,
+        }
+      }
+    )
+  }
+}
+</script>
---
+++ src/components/DoughnutChartRemainingOnBoard.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container">
+      <div class="chart">
+        <DoughnutChart
+          :chartData="chartData"
+          :width="150"
+          :height="150"
+        />
+      </div>
+    </div>
+</template>
+
+<script>
+import DoughnutChart from '@/components/DoughnutChart'
+import colorPalette from '@/constants/colorPalette'
+
+const { COLOR_BLACK, COLOR_BROWN, COLOR_BLUE, COLOR_WHITE } = colorPalette
+
+export default {
+  components: {
+    DoughnutChart
+  },
+
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+
+  computed: {
+    chartData () {
+      return {
+        labels: [
+          'Filled',
+          'Empty'
+        ],
+        datasets: [
+          {
+            data: [parseInt(this.item.header), 10],
+          }
+        ]
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang='scss'>
+.container {
+  .chart {
+    background: transparent;
+    padding: 20px;
+    text-align: center;
+
+    &__header {
+      margin-top: 0;
+      padding: 15px 0;
+      color:  $color-dark-grey;
+    }
+  }
+}
+</style>
