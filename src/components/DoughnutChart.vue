7a8c3e11c8fc9e18ab7feb9571be8e25631b55dd
Mateusz Rybczonek <mateusz.rybczonek@netguru.pl>
2018-06-12 12:16:31 +0200

    add reactiveProp mixin to DoughnutChart

--- src/components/DoughnutChart.vue
+++ src/components/DoughnutChart.vue
@@ -1,8 +1,10 @@
 <script>
-import { Doughnut } from 'vue-chartjs'
+import { Doughnut, mixins } from 'vue-chartjs'
+const { reactiveProp } = mixins
 
 export default {
   extends: Doughnut,
+  mixins: [reactiveProp],
 
   props: {
     chartData: {
