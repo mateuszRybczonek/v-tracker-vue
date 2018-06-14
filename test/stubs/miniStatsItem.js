dfdedc625e124341d3884468aa40a3c9ed42caf6
Mateusz Rybczonek <mateusz.rybczonek@netguru.pl>
2018-06-14 09:58:54 +0200

    adjust MiniStatsItemBody unit test

--- test/stubs/miniStatsItem.js
+++ test/stubs/miniStatsItem.js
@@ -8,4 +8,15 @@ const miniStatsItem = {
   unit: 'cbm'
 }
 
-export { miniStatsItem }
+const miniStatsItemWithFreeSpace = {
+  change: 0,
+  color: 'black',
+  customClass: 'mini-stats__item--fo',
+  description: 'Fuel Oil',
+  header: '90',
+  icon: 'IconFuel',
+  unit: 'cbm',
+  freeSpace: 90
+}
+
+export { miniStatsItem, miniStatsItemWithFreeSpace }
