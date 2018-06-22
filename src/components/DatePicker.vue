a5214c186be2cd0a30045f5c271628fc11413d71
Mateusz Rybczonek <mateusz.rybczonek@netguru.pl>
2018-06-22 21:39:09 +0200

    add datepicker controllable component

---
+++ src/components/DatePicker.vue
@@ -0,0 +1,42 @@
+<template>
+  <input :value="date" ref="datepicker" :placeholder="placeholder">
+</template>
+
+<script>
+import Pikaday from 'pikaday'
+import 'pikaday/css/pikaday.css'
+
+export default {
+  model: {
+    prop: 'date',
+    event: 'selected'
+  },
+
+  props: {
+    date: {
+      required: true
+    },
+
+    format: {
+      type: String,
+      default: 'YYYY-MM-DD'
+    },
+
+    placeholder: {
+      type: String,
+      default: 'YYYY-MM-DD'
+    }
+  },
+
+  mounted() {
+    const picker = new Pikaday({
+      field: this.$refs.datepicker,
+      format: this.format,
+
+      onSelect: () => {
+        this.$emit('selected', picker.toString())
+      }
+    })
+  }
+}
+</script>
